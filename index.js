b951f968d8d57389b85ff28a3204e50a4267c5a9
rodrigograca31 <rodrigograca1993@gmail.com>
2020-01-13 13:11:06 +0000

    📦 Setup

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // implement your API here
+const express = require("express");
+const cors = require("cors");
+const { find, findById, add, remove, update } = require("./data/db");
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(3000, () => {
+	console.log("listening on 3000");
+});
